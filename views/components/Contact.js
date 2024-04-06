6f364036de473cb2430f1b542be8d8b14de5cad7
Ryan Duer <ryan@allmaddesigns.com>
2024-04-06 12:24:57 -0700

    initial layout done

---
+++ views/components/Contact.js
@@ -0,0 +1,37 @@
+const Contact = () => {
+	return (
+		<>
+			<section className="contact first">
+				<h1>Software Engineer</h1>
+				<p className="header-hide">2 Years Experience</p>
+			</section>
+			<section className="contact second">
+			<h2 className="header-hide">Skills</h2>
+				<p className="header-hide">
+					JavaScript | React Typescript | Redux | Node | SCSS/CSS |
+					PHP | WCAG | NPM | CircleCI | WordPress | Gutenberg
+				</p>
+			</section>
+			<section className="contact third">
+				<h2 className="header-hide">Contact</h2>
+				<p>
+					<a
+						href='https://www.linkedin.com/in/ryan-duer/'
+						target='_blank'
+					>
+						<img src="/LinkedIn.png" alt="to github profile" />
+					</a>
+				</p>
+				<p>
+					<a
+						href='https://github.com/ryanAllMad'
+						target='_blank'
+					>
+						<img src="/github-mark.png" alt="to github profile" />
+					</a>
+				</p>
+			</section>
+		</>
+	);
+};
+export default Contact;
--- views/goals.js
+++ views/goals.js
@@ -1,12 +1,59 @@
+import Contact from './components/Contact.js';
+
 export default function GoalsPage() {
 	return (
-		<main>
-			<section>
-				uno
-			</section>
-			<section>
-				dose
-			</section>
-		</main>
+		<>
+			<head>
+				<meta charSet='utf-8' />
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1'
+				/>
+				<link
+					rel='preconnect'
+					href='https://fonts.googleapis.com'
+				/>
+				<link
+					rel='preconnect'
+					href='https://fonts.gstatic.com'
+					crossOrigin="anonymous"
+				/>
+				<link
+					href='https://fonts.googleapis.com/css2?family=Readex+Pro:wght@160..700&display=swap'
+					rel='stylesheet'
+				/>
+				<link
+					rel='stylesheet'
+					href='/styles.css'
+				></link>
+			</head>
+			<body>
+				<header className='desktop-hidden'>
+					<section className='logo'></section>
+					<Contact />
+				</header>
+				<main>
+					<section className='mobile-hidden logo'></section>
+					<section className='project one'>
+						<img src='/dummy.jpg' alt="dummy image" />
+					</section>
+					<section className='project two'>
+						<img src='/dummy.jpg' alt="dummy image" />
+					</section>
+					<section className='project three'>
+						<img src='/dummy.jpg' alt="dummy image" />
+					</section>
+					<section className='project four'>
+					<img src='/dummy.jpg' alt="dummy image" />
+					</section>
+				</main>
+				<footer>
+					<Contact />
+					<aside style={{fontSize: '12px'}}>
+					Images By LinkedIn -  <a rel="nofollow" href="https://developer.linkedin.com/documents/branding-guidelines"> https://developer.linkedin.com/documents/branding-guidelines </a> Public Domain, <a href="https://commons.wikimedia.org/w/index.php?curid=30784763">Link</a>
+					</aside>
+				</footer>
+			</body>
+		</>
 	);
 }
