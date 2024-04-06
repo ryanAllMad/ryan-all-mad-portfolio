fd8e3d43d56291452d7336fc0b78fd88dfa683b7
Ryan Duer <ryan@allmaddesigns.com>
2024-04-06 13:34:40 -0700

    clean up click" "

--- public/js/index.js
+++ public/js/index.js
@@ -1,36 +1,42 @@
 const logoHead = document.querySelector('header .logo');
 
-const renderAboutTNode = () => {
-	const skillsArr = [
-		`JavaScript`,
-		`React`,
-		`Typescript`,
-		`Redux `,
-		`Node `,
-		`SCSS/CSS`,
-		`PHP`,
-		`WCAG`,
-		`NPM`,
-		`CircleCI`,
-		`WordPress`,
-		`Gutenberg`,
-	];
-	const skillList = document.createElement('ul')
-	const aboutDropdown = document.createElement('div');
-	const heading = document.createElement('h2')
-	const headingText = document.createTextNode('Skills')
-	skillsArr.forEach((sk) => {
-		const list = document.createElement('li')
-		const listText = document.createTextNode(sk)
-		list.appendChild(listText)
-		skillList.appendChild(list)
-	})
-	
-	aboutDropdown.className = 'show-skills';
-	heading.appendChild(headingText)
-	aboutDropdown.appendChild(heading)
-	aboutDropdown.appendChild(skillList)
-	logoHead.insertAdjacentElement('beforeend', aboutDropdown)
+const renderAboutTNode = (element) => {
+	const elementClass = document.querySelector(element)
+	if (elementClass) {
+		elementClass.remove()
+	}
+	if(!elementClass) {
+		const skillsArr = [
+			`JavaScript`,
+			`React`,
+			`Typescript`,
+			`Redux `,
+			`Node `,
+			`SCSS/CSS`,
+			`PHP`,
+			`WCAG`,
+			`NPM`,
+			`CircleCI`,
+			`WordPress`,
+			`Gutenberg`,
+		];
+		const skillList = document.createElement('ul')
+		const aboutDropdown = document.createElement('div');
+		const heading = document.createElement('h2')
+		const headingText = document.createTextNode('Skills')
+		skillsArr.forEach((sk) => {
+			const list = document.createElement('li')
+			const listText = document.createTextNode(sk)
+			list.appendChild(listText)
+			skillList.appendChild(list)
+		})
+		
+		aboutDropdown.className = 'show-skills';
+		heading.appendChild(headingText)
+		aboutDropdown.appendChild(heading)
+		aboutDropdown.appendChild(skillList)
+		logoHead.insertAdjacentElement('beforeend', aboutDropdown)
+	}
 };
 
 const hideOnScroll = (el) => {
@@ -41,5 +47,5 @@ const hideOnScroll = (el) => {
 	elementClass.remove()
 }
 
-logoHead.addEventListener('click', renderAboutTNode);
+logoHead.addEventListener('click', (event) => renderAboutTNode('header .logo .show-skills'));
 window.addEventListener('scroll', (event) => hideOnScroll('header .logo .show-skills'))
