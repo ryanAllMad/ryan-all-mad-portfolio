109cf167aac0d64c685c3bdb322df97ba1f941e0
Ryan Duer <ryan@allmaddesigns.com>
2024-04-08 18:12:24 -0700

    update port

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-import express from 'express'
+import express from 'express';
 import { renderToPipeableStream } from 'react-dom/server';
 import GoalsPage from './views/GoalsPage.js';
-import process from 'process'
+import process from 'process';
 
-//nvm use 21.7.2 
+//nvm use 21.7.2
 const app = express();
 
-app.set('views', './views')
+app.set('views', './views');
 
-
-app.use(express.urlencoded({extended: false}))
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 app.use('/', (request, response) => {
 	const { pipe } = renderToPipeableStream(<GoalsPage />, {
-	onShellReady() {
-		response.setHeader('content-type', 'text/html');
-		pipe(response);
-	}
+		onShellReady() {
+			response.setHeader('content-type', 'text/html');
+			pipe(response);
+		},
 	});
 });
+function normalizePort(val) {
+	var port = parseInt(val, 10);
 
-app.listen(process.env.port || 3000)
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
+
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+
+	return false;
+}
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+app.listen(port);
